af92d58f4cadf17616086f3235e7d583b8bca71f
Lorenzo Bertozzi <37419347+LorenzoBertozzi@users.noreply.github.com>
2024-07-16 14:17:45 -0300

    Update Grafo.h

--- listaadj/Grafo.h
+++ listaadj/Grafo.h
@@ -7,7 +7,6 @@
 #include <array>
 #include <cstring>
 #include <float.h>
-#include <stack>
 using namespace std;
 
 	class Grafo {
@@ -15,7 +14,6 @@ using namespace std;
 		class Aresta {
 	  private:
 	    int v1, v2, peso; 
-
 	  public: 
 	    Aresta (int v1, int v2, int peso) {
 	      this->v1 = v1; this->v2 = v2; this->peso = peso;
@@ -24,6 +22,9 @@ using namespace std;
 	    int _v1 () { return this->v1; }
 	    int _v2 () { return this->v2; }
 	    ~Aresta(){}
+
+      //sobreescrita operador "<"
+      bool operator < (const Aresta& aresta2) const {return (peso < aresta2.peso);}
 	  };
 	private:	
 		class Celula {
@@ -54,8 +55,6 @@ using namespace std;
 	  }; 
     Lista<Celula> *adj; 
     int numVertices;
-    vector <int> conjunto;
-
   public:
     Grafo( istream &in );
     Grafo (int numVertices);	  
@@ -70,29 +69,12 @@ using namespace std;
 	  void imprime () const ;
 	  int _numVertices () const;
 	  Grafo *grafoTransposto ();
-    ~Grafo ();	
-
-    void buscaEmLargura ();
-    void visitaBfs (int u, vector <string>& cor, vector <int>&dist, vector <int>&antecessor);
-    void buscaEmProfundidade();
-    void visitaDFS(int u, vector<string>& cor, vector<int>& antecessor);
-    bool temCiclo(); // verifica se o grafo tem cilho 
-    bool visitaDFSComCiclo(int u, vector<string>& cor);//busca ciclo do grafo
-    int contarComponentes(); //retorna quantidade de vertices
-    void visitaDfsTopologica(int u, vector<string>& cor, stack<int>& pilha);
-    void ordenacaoTopologica();//Mostrar uma ordenação topológica
-    void imprimeCaminho (int u, int v);
+    ~Grafo ();	  
 
     void kruskal();
-    void criaConjunto();
-    int encontreConjunto(int x);
-    void unirConjunto(int x, int y);
-
-    void prim(int raiz);
 	};
 
-  Grafo::Grafo( istream &in )
-  {
+  Grafo::Grafo( istream &in ){
    int v1, v2, peso, numVertices;
    in >> numVertices;
   
@@ -163,8 +145,8 @@ using namespace std;
       cout << endl;
     }
   }
-  int Grafo::_numVertices () const { 
-    return this->numVertices; 
+  int Grafo::_numVertices () const {
+     return this->numVertices;
   }
   Grafo *Grafo::grafoTransposto () {  	
     Grafo *grafoT = new Grafo (this->numVertices); 
@@ -181,276 +163,67 @@ using namespace std;
   }
   Grafo::~Grafo () {
     delete [] this->adj;
-  }	  
-  //BUSCA EM LARGURA
-  void Grafo::buscaEmLargura(){
-    vector <string> cor (numVertices, "branco");
-    vector <int> dist (numVertices, INT8_MAX);
-    vector <int> antecessor(numVertices, -1);
-
-    for (int u = 0; u < numVertices; u++){
-      if(cor[u] == "branco"){
-        visitaBfs(u, cor, dist, antecessor);
-      }
-    }
-  }	
-  void Grafo::visitaBfs(int u, vector <string>& cor, vector <int>&dist, vector <int>&antecessor){
-    dist[u] = 0;
-    cor[u] = "cinza";
-
-    //cout << "-" << u << " cor: " << cor[u] << " dist: " << dist[u] << " ant: " << antecessor[u] << endl;
-
-    queue<int> fila;
-    fila.push(u);
-
-    while (!fila.empty()){
-      u = fila.front();
-      fila.pop();
-      Aresta* adj = primeiroListaAdj(u);
-      while (adj != NULL){
-        int v= adj->_v2();
-        if(cor[v] == "branco"){
-          cor[v] = "cinza";
-          dist[v] = dist[u] + 1;
-          antecessor[v] = u;
-          fila.push(v);
-
-          //cout << "-" << v << " cor: " << cor[v] << " dist: " << dist[v] << " ant: " << antecessor[v] << endl;
-        }
-        adj = proxAdj(u);
-      }
-      cor[u] = "preto";
-
-      //cout << "-" << u << " cor: " << cor[u] << " dist: " << dist[u] << " ant: " << antecessor[u]  << "\n" << endl;
-    }
-  }
-  //BUSCA EM PORFUNDIDADE
-  void Grafo::buscaEmProfundidade() {
-    vector<string> cor(numVertices, "branco");
-    vector<int> antecessor(numVertices, -1);
-
-    for (int u = 0; u < numVertices; u++) {
-        if (cor[u] == "branco") {
-          //cout << u << " cor : " << cor[u] << " antecessor : " << antecessor[u] << endl;
-          visitaDFS(u, cor, antecessor);
-        }
-    }
-  }
-  void Grafo::visitaDFS(int u, vector<string>& cor, vector<int>& antecessor) {
-    cor[u] = "cinza";
-    Aresta* adj = primeiroListaAdj(u);
-    //cout << u << " cor : " << cor[u] << " antecessor : " << antecessor[u] << endl;
-    while (adj != NULL) {
-        int v = adj->_v2();
-        if (cor[v] == "branco") {
-          antecessor[v] = u;          
-          visitaDFS(v, cor, antecessor);
-          //cout << v << " cor : " << cor[v] << " antecessor : " << antecessor[v] << endl;
-        }
-        adj = proxAdj(u);
-    }
-
-    cor[u] = "preto";
-  }
-  //VERIFICA SE O GRAFO POSSUI CICLO ULTILIZANDO DFS
-  bool Grafo::temCiclo() {
-    vector<string> cor(numVertices, "branco");
-
-    for (int u = 0; u < numVertices; u++) {
-        if (cor[u] == "branco") {
-            if (visitaDFSComCiclo(u, cor))
-                return true; // Se encontrou um ciclo, retorna verdadeiro
-        }
-    }
-
-    return false; // Se não encontrou ciclos, retorna falso
-  }
-  bool Grafo::visitaDFSComCiclo(int u, vector<string>& cor) {
-    cor[u] = "cinza";
-    Aresta* adj = primeiroListaAdj(u);
-
-    while (adj != NULL) {
-        int v = adj->_v2();
-        if (cor[v] == "cinza")
-            return true; // Se encontrou um vértice cinza, significa que há um ciclo
-
-        if (cor[v] == "branco" && visitaDFSComCiclo(v, cor))
-            return true; // Se encontrou um ciclo em uma chamada recursiva, retorna verdadeiro
-
-        adj = proxAdj(u);
-    }
-
-    cor[u] = "preto";
-    return false; // Não encontrou ciclo a partir deste vértice
-  }
-  //VERIFICAR QUANTIDADE DE COMPONETES DO GRAFO (QUANTIDADE DE GRAFOS)
-  int Grafo::contarComponentes() {
-    vector<string> cor(numVertices, "branco");
-    vector<int> antecessor(numVertices, -1);
-    int numComponentes = 0;
-    for (int u = 0; u < numVertices; u++) {
-        if (cor[u] == "branco") {
-          numComponentes++;
-          visitaDFS(u, cor, antecessor);
-        }
-    }
-    return numComponentes;
-  } 
-  //MOSTRA O CAMINHO MAIS CURTO ENTRE DOIS VERTICES
-  void Grafo::imprimeCaminho(int u, int v){
-    vector <string> cor (numVertices, "branco");
-    vector <int> dist (numVertices, INT8_MAX);
-    vector <int> antecessor(numVertices, -1);
-
-    visitaBfs(u, cor, dist, antecessor);
-
-    if (u == v) {
-        cout << v << " " ; 
-    } else if (antecessor[v] == -1) {
-        cout << "Não existe caminho de " << u << " para " << v << endl;
-    } else {
-        imprimeCaminho(u, antecessor[v]); 
-        cout << v << " "; 
-    }
-  }
-  //MOSTRAR UMA ORDENAÇÃO TOPOLOGICA PARA O GRAFO
-  /*
-  void Grafo::visitaDfsTopologica(int u, vector<string>& cor, stack<int>& pilha) {
-    cor[u] = "cinza";
-
-    Aresta* adj = primeiroListaAdj(u);
-    while (adj != NULL) {
-        int v = adj->_v2();
-        if (cor[v] == "branco") {
-            visitaDfsTopologica(v, cor, pilha);
-        }
-        adj = proxAdj(u);
-    }
-
-    cor[u] = "preto";
-    pilha.push(u);
-}
-  void Grafo::ordenacaoTopologica(){
-    vector<string> cor(numVertices, "branco");
-    stack <int> pilha;
-
-    for(int i = 0; i < numVertices; i++){
-      if(cor[i] == "branco"){
-        visitaDfsTopologica(i , cor, pilha);
-      }
-    }
-
-    //imprimir ordenação topologica
-    cout << "Ordenação topologica : ";
-    while (!pilha.empty()){
-      cout << pilha.top() << " ";
-      pilha.pop();
-    }
-    cout << endl;
-  }
-  //CRIR UMA ARVORE GERADORA MINIMA USANDO ALGORITIMO DE KRUSKAL
-  void Grafo::kruskal(){
-    vector<Grafo::Aresta*> arestasAGM;
-    criaConjunto();
-
-    vector<pair<int, pair<int, int>>> arestaOrdenas;;
-    for(int u = 0; u < numVertices; u++){
-      Grafo::Aresta* adj = primeiroListaAdj(u);
-      while (adj != nullptr){
-        int v = adj->_v2();
-        int peso = adj->_peso();
-        arestaOrdenas.push_back({peso, {u,v}});
-        adj = proxAdj(u);
-      }
-    }
-    sort(arestaOrdenas.begin(), arestaOrdenas.end());
-
-    for (auto & aresta : arestaOrdenas){
-      int u = aresta.second.first;
-      int v = aresta.second.second;
-      if(encontreConjunto(u) != encontreConjunto(v)){
-        arestasAGM.push_back(new Grafo::Aresta(u, v, aresta.first));
-        unirConjunto(u,v);
-      }
-    }
-    
-    cout << "Árvore Geradora Mínima (AGM) encontrada por Kruskal:" << endl;
-    for(auto& aresta : arestasAGM){
-              cout << "(" << aresta->_v1() << ", " << aresta->_v2() << ") - Peso: " << aresta->_peso() << endl;
-    }
-  }
-
-  void Grafo::criaConjunto(){
-    for (int v = 0; v < numVertices; ++v) {
-        conjunto[v] = -1;
-    }
-  }
+  }	 
   
-  int Grafo::encontreConjunto(int x){
-    if(conjunto[x] == -1){
-      return x;
-    }
-    return encontreConjunto(conjunto[x]);
-  }
-
-  void Grafo::unirConjunto(int x, int y){
-    int conjuntoX = encontreConjunto(x);
-    int conjuntoY = encontreConjunto(y);
-    conjunto[conjuntoX] = conjuntoY;
-  }
-  */
-
-//CRIA UM ARVORE GERADORA MINIMA ULTILIZANDO ALGORITMO PRIM
-  void Grafo::prim(int raiz) {
-    const int INF = INT8_MAX; // Definindo infinito como o maior valor possível
-    double* peso = new double[numVertices]; // Array para armazenar os pesos dos vértices
-    int* antecessor = new int[numVertices]; // Array para armazenar os antecessores dos vértices na AGM
-    bool* itensHeap = new bool[numVertices]; // Array para rastrear se um vértice está no heap ou não
-    int* vs = new int[numVertices + 1]; // Array para heap indireto
-
-    for (int u = 0; u < numVertices; ++u) {
-        peso[u] = INF; // Inicializa os pesos como infinito
-        antecessor[u] = -1; // Inicializa os antecessores como -1 (indicando nenhum antecessor)
-        itensHeap[u] = true; // Inicializa todos os vértices como presentes no heap
-        vs[u + 1] = u; // Inicializa o heap indireto
-    }
-
-    peso[raiz] = 0; // Define o peso da raiz como 0
-
-    FPHeapMinIndireto<double> Q(peso, vs, numVertices); // Criação do heap
-
-    while (!Q.vazio()) {
-        int u = Q.retiraMin(); // Retira o vértice de menor peso do heap
-        itensHeap[u] = false; // Marca o vértice como retirado do heap
-
-        Aresta* adj = primeiroListaAdj(u); // Obtém as arestas adjacentes ao vértice u
-        while (adj != nullptr) {
-            int v = adj->_v2(); // Obtém o segundo vértice da aresta (o vértice adjacente)
-            int pesoAresta = adj->_peso(); // Obtém o peso da aresta
-
-            if (itensHeap[v] && pesoAresta < peso[v]) {
-                antecessor[v] = u; // Define o vértice u como antecessor de v
-                Q.diminuiChave(v, pesoAresta); // Atualiza o peso de v no heap
-            }
-
-            adj = proxAdj(u); // Passa para a próxima aresta adjacente
+  class encontrarUniao{
+    private:
+      vector<int> parent, rank;
+    public:
+      encontrarUniao(int n) : parent(n), rank(n,0){
+        for(int i = 0; i < n; i++){
+          parent[i]=i;
         }
-    }
-
-    // Imprime a Árvore Geradora Mínima encontrada por Prim
-    cout << "Árvore Geradora Mínima (AGM) encontrada por Prim:" << endl;
-    for (int v = 0; v < numVertices; ++v) {
-        if (antecessor[v] != -1) { // Se o vértice tem um antecessor na AGM
-            cout << "(" << antecessor[v] << ", " << v << ")" << endl; // Imprime a aresta (antecessor[v], v)
+      }
+      int encontrar(int u){
+        if(u != parent[u])
+          parent[u] = encontrar(parent[u]);
+        return parent[u];
+      }
+      void uniao(int u, int v){
+        int rootU = encontrar(u);
+        int rootV = encontrar(v);
+        if(rootU != rootV){
+          if(rank[rootU] != rank[rootV]){
+            parent[rootV] = rootU;
+          }
+          else if(rank[rootU] < rank[rootV]){
+            parent[rootU] = rootV;
+          }
+          else{
+            parent[rootV] = rootU;
+            rank[rootU]++;
+          }
         }
+      }
+  };
+
+  void Grafo::kruskal(){
+    vector <Aresta> todasArestas;
+    for (int v = 0; v < this->numVertices; v++){
+      Aresta *adj = this->primeiroListaAdj(v);
+      while (adj != NULL){
+        todasArestas.push_back(*adj);
+        delete adj;
+        adj = this->proxAdj(v);
+      } 
     }
 
-    // Libera memória alocada dinamicamente
-    delete[] peso;
-    delete[] antecessor;
-    delete[] itensHeap;
-    delete[] vs;
-}
+    sort(todasArestas.begin(), todasArestas.end());
 
+    encontrarUniao uf (this->numVertices);
+    vector <Aresta> AGM;
 
+    for(auto& aresta : todasArestas){
+      int u = aresta._v1();
+      int v = aresta._v2();
+      if (uf.encontrar(u) != uf.encontrar(v)){
+        uf.uniao(u, v);
+        AGM.push_back(aresta);
+      }
+    }
 
+    cout << "Aresta da AGM :" << endl;
+    for(auto& aresta : AGM){
+      cout << "(" << aresta._v1() << "," << aresta._v2() << ")peso: " << aresta._peso() << endl;
+    }
+  }
