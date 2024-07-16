f95120dce8d4a00a4314f6c2f6d488c678ad96ec
Lorenzo Bertozzi <37419347+LorenzoBertozzi@users.noreply.github.com>
2024-07-16 14:18:17 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,25 +8,7 @@ int main (int argc, char **argv)
     Grafo *grafo = new Grafo (in);
     grafo->imprime ();  
 
-    //grafo->buscaEmLargura();
-    //grafo->buscaEmProfundidade();
-
-    //1- Verificar se o grafo possui ciclos.
-    if(grafo->temCiclo()){
-        cout << "ciclico" << endl;
-    }
-    else{
-        cout << "nao tem ciclo" << endl;
-    }
-
-    //2- Verificar a quantidade de componentes do grafo.
-    int numComponentes = grafo->contarComponentes();
-    cout << "O grafo possui " << numComponentes << " componentes." << endl;
-
-    //grafo->imprimeCaminho(0,6);
-
-    grafo->ordenacaoTopologica();
-
     grafo->kruskal();
 
+    return 0;
 }
